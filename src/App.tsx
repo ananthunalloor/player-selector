14f64dca39f7827d77d5fafb7b27e2967a9400e4
ananthunalloor <ananthunalloor@gmail.com>
2023-12-06 11:46:18 +0530

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import SlotCounter from "react-slot-counter";
+
 function App() {
-  return <div className="container"></div>;
+  const [value, setValue] = useState(0);
+
+  const onClickHandler = () => {
+    //on click change the value to random number between 0 and 350
+    setValue(Math.floor(Math.random() * 350));
+  };
+
+  return (
+    <div className="flex">
+      <SlotCounter
+        value={value}
+        startValue="000"
+        startValueOnce
+        animateUnchanged
+        duration={3}
+        autoAnimationStart={false}
+      />
+      <button onClick={onClickHandler}>Click</button>
+    </div>
+  );
 }
 
 export default App;
