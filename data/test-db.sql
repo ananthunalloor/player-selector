8fa46c1c2bcac7c27b16296f2f255ac91eb3d964
ananthunalloor <ananthunalloor@gmail.com>
2023-12-17 08:41:27 +0530

    added sql

--- data/test-db.sql
+++ data/test-db.sql
@@ -1,9 +1,6 @@
--- Create a new database
-CREATE DATABASE IF NOT EXISTS `testdb`;
 
-
--- Use the database
-USE `testdb`;
+-- Enable foreign key constraints
+PRAGMA foreign_keys = ON;
 
 -- Create a tournament table
 CREATE TABLE IF NOT EXISTS tournaments (
@@ -13,26 +10,30 @@ CREATE TABLE IF NOT EXISTS tournaments (
   end_date TEXT NOT NULL
 );
 
-
 -- Create a team table
 CREATE TABLE IF NOT EXISTS teams (
   team_id INTEGER PRIMARY KEY,
-  FOREIGN KEY (tournament_id) REFERENCES tournaments(tournament_id),
+  tournament_id INTEGER NOT NULL,
   name TEXT NOT NULL,
-  points INTEGER NOT NULL
+  points INTEGER NOT NULL,
+  FOREIGN KEY (tournament_id) REFERENCES tournaments(tournament_id)
 );
 
 -- Create a player table
 CREATE TABLE IF NOT EXISTS players (
   player_id INTEGER PRIMARY KEY,
-  FOREIGN KEY (team_id) REFERENCES teams(team_id),
+  team_id INTEGER NOT NULL,
   name TEXT NOT NULL,
-  points INTEGER NOT NULL
+  points INTEGER NOT NULL,
+  FOREIGN KEY (team_id) REFERENCES teams(team_id)
 );
 
--- Create a team-list table
+-- Create a team_list table
 CREATE TABLE IF NOT EXISTS team_list (
-  FOREIGN KEY (team_id) REFERENCES teams(team_id),
-  FOREIGN KEY (player_id) REFERENCES players(player_id),
+  team_id INTEGER,
+  player_id INTEGER,
   points INTEGER NOT NULL,
+  PRIMARY KEY (team_id, player_id),  -- Add a primary key constraint if needed
+  FOREIGN KEY (team_id) REFERENCES teams(team_id),
+  FOREIGN KEY (player_id) REFERENCES players(player_id)
 );
